ec3dd95f9dc08b7f95d9768c303446667aa7cf6e
Jasiel Campos <jasielcamposromero@gmail.com>
2023-10-19 11:55:53 -0400

    fix

--- resources/views/book/index.blade.php
+++ resources/views/book/index.blade.php
@@ -16,7 +16,7 @@
         <div class="message"></div>
         <table class="content-table">
           <thead>
-            <th>S.No</th>
+            <th>Nro.</th>
             <th>Titulo</th>
             <th>Categoría</th>
             <th>Autor</th>
